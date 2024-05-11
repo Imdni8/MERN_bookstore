a507e6962b267acb420157c71a8dfd439761657f
Imdni8 <tousif.rahaman@gmail.com>
2024-05-11 17:01:19 +0530

    preparing for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,19 @@
 // import axios from "axios"
-import {Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
-import AddNew from "./pages/AddNew"
-import EditBook from "./pages/EditBook"
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AddNew from "./pages/AddNew";
+import EditBook from "./pages/EditBook";
 
 function App() {
-// axios.get("http://localhost:3000/books")
-// .then(res => console.log(res.data[0].title))
-
-  return (
-    <>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/AddNew" element={<AddNew/>} />
-      <Route path='/EditBook/:id' element={<EditBook/>} />
-    </Routes>
-    </>
-    
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/AddNew" element={<AddNew />} />
+                <Route path="/EditBook/:id" element={<EditBook />} />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
